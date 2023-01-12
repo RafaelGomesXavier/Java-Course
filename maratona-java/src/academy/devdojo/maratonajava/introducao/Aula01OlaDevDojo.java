f69ec2db580c6b2055bfe3191b8c779c045c5288
Não logarias <contato.rafaelgomes@outlook.com>
2023-01-12 11:56:59 -0300

    a

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * isso é um comentário <b>Javadoc</b>
+ */
+public class Aula01OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("Ka-me-ha-me-haaaa");
+    }
+}
